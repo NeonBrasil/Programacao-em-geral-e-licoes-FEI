fa6609d93340b5ac3b0ead75895867b9ab61f07c
Cayque <cayquecicarelli@gmail.com>
2022-03-09 17:55:31 -0300

    COBOL
    Hello world em COBOL

---
+++ COBOL/helloworld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Cayque Cicarelli
+      * Date: 07/03/2022
+      * Purpose: study
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM HELLOWORLD.
