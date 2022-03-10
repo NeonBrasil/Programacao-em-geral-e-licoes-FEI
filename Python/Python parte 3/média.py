2ab262dd644d4e347a1189fdcb371ce5af56d443
Cayque <cayquecicarelli@gmail.com>
2022-03-10 13:20:29 -0300

    Create média.py

---
+++ Python/Python parte 3/média.py
@@ -0,0 +1,9 @@
+def media(a, b, c):
+   return ((a +b + c)/3)
+
+#Escreva a função media() que recebe três números
+#  como parâmetros e retorna o valor médio desses 
+# parâmetros como seu resultado. 
+# Escreva somente a função solicitada; 
+# não é necessário o programa principal.
+
