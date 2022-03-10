7365623fcb3a86d1af8ab4b57a7429fbae8242ec
Cayque <cayquecicarelli@gmail.com>
2022-03-10 13:20:57 -0300

    Create produtório.py

---
+++ Python/Python parte 3/produtório.py
@@ -0,0 +1,15 @@
+def produtorio(num1, num2, num3, num4=1, num5=1, num6=1):
+    produto = num1 * num2 * num3 * num4 * num5 * num6
+    return produto
+
+#Crie uma função chamada produtorio 
+# que realize o cálculo do produtório, 
+# conforme equação abaixo:
+#(x + a) (x + b) (x + c) … (x + n) = 0
+#Sua função deverá receber entre 3 e 6 parâmetros
+#  e retornar o valor do produtório. 
+# Não é necessário fazer o programa principal.
+
+#Produtório é a multiplicação de uma 
+# sequência de objetos matemáticos 
+# (números, funções, vetores, matrizes, etc.).
