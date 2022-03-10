bdc398f0b352c29bc4ce3a4ada3378105f6b5056
Cayque <cayquecicarelli@gmail.com>
2022-03-10 13:20:15 -0300

    Create lista.py

---
+++ Python/Python parte 3/lista.py
@@ -0,0 +1,7 @@
+Z = [7, 8, 9,"opa", 5]
+Z .append("uga")
+Z .insert(1, "tchau")
+#pop serve pra apagar algo, se tiver [a,2,3] e colocar Z .pop(1) vc remove o "a"
+#a unção remove tbm funciona, se colocar .remove (1) e a lista for [1,2,3,1,1] ele vai deixar como [2,3,1,1] pois só remove o primeiro q aparece em ordem
+
+print(Z)
