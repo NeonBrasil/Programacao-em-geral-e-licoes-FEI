f7cc1cfe982fbe3c653aa066be7cd444c253efe1
Cayque <cayquecicarelli@gmail.com>
2022-03-11 12:33:38 -0300

    Update java

---
+++ Java/JAVA parte 2/loop.java
@@ -0,0 +1,33 @@
+public class loop {
+    
+    public static void main (String[] args){
+
+        //loops in Java
+        //Get the squares of the first 5 intergers
+        // forma mais idiota, um por um
+
+
+        //System.out.println(1 + " squared = " +(1*1));
+        //System.out.println(1 + " squared = " +(2*2));
+        //System.out.println(1 + " squared = " +(3*3));
+        //System.out.println(1 + " squared = " +(4*4));
+        //System.out.println(1 + " squared = " +(5*5));
+
+
+        // usando For Loop
+        // for( <inicialização> ; <continuação teste>; , <update>{
+//codigo que vc quer q se repita
+//
+    //    }
+
+// i=5 = i<6
+    for (int i = 1; i <= 5; i++){
+
+        System.out.println( i + " squared = " + (i*i) );
+    }
+
+
+
+
+    }
+}
---
+++ Java/JAVA parte 2/variaveis_escolhendo.java
@@ -0,0 +1,47 @@
+public class variaveis_escolhendo {
+
+
+    public static void main (String[] args){
+
+
+        //int x ;
+        //double y ;
+
+        //x = 2+3;  //=5 é inteiro
+        //y= 3.4*2.9; // xxx.xxx double
+
+        // vc n pode asing uma dupla em um inteiro
+        // mas vc pode assing um inteiro em dupla
+
+        //y = 2+3; funciona
+        // x = 3.4*2.9; não funciona
+
+        // y = 2+3; = 5.0 Java promove o inteiro
+        // no double
+
+        //x = 3.4*2.9; = 9.86 (n é inteiro)
+        // vc teria q falar para o Java
+        // transformar em double, como?
+        //x = (int) (3.4*2.9);// Agora vai
+        //dar só a parte inteira, então
+        //ao inves de dar 9.86, vai dar 9
+
+        //System.out.print(" "x);
+
+        // int x, y , z; // int x;
+                        // int y;
+                        // int z;
+        
+        //int x = 5+1 + 8*1; //operadores vão da 
+        //esquerda pra direita
+
+        // int x,y,z;
+        // assignment them variaveis da direita
+        //para a esquerda
+        //x=y=z=2*5 +4;
+        
+
+
+    }
+    
+}
